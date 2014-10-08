68533ef8518f1705d454f24f3d9cbba53e95b223
Guarionex Robiou <grobiou@gmail.com>
2014-10-08 19:42:35 -0400

    Back to last gems

--- config/initializers/paperclip.rb
+++
@@ -1,3 +0,0 @@
-# config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':s3_host_name'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
