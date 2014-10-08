c2349cf92fbb15385b24bba64da77b908aeb4f3d
Guarionex Robiou <grobiou@gmail.com>
2014-10-08 01:45:20 -0400

    added paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':gary-pinteresting.s3-website-us-east-1.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = 'gary-pinteresting.s3-website-us-east-1.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
